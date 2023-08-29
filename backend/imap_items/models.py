b37d9c5712763a4e2deb936438771d87357a2e94
Krzysztof Olszewski <hello@retryjoin.pl>
2023-08-29 18:16:21 +0200

    ...

--- backend/imap_items/models.py
+++ backend/imap_items/models.py
@@ -1,9 +1,11 @@
+import csv
+
 from django.db import models
 from PIL import Image
 
 
 class ImapItem(models.Model):
-    csv = models.FileField(upload_to='files')
+    csv_file = models.FileField(upload_to='files')
     result = models.CharField(max_length=250, blank=True)
     date_uploaded = models.DateTimeField(auto_now_add=True)
 
@@ -14,6 +16,9 @@ class ImapItem(models.Model):
         try:
 
             self.result = str("decoded")
+            csv_data = csv.reader(self.csv_file.open(mode='rb'))
+            for item in csv_data:
+                print(item)
             print('Success')
         except Exception as e:
             print('Classification failed:', e)
